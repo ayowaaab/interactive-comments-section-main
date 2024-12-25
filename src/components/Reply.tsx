19cbc891690e44a0e7e03e0a7eda5478a849de7f
ayowaaab <ayoub1dahmen@gmail.com>
2024-12-25 09:17:45 +0100

    refactor Reply component layout; enhance textarea and button styles

--- src/components/Reply.tsx
+++ src/components/Reply.tsx
@@ -1,14 +1,23 @@
+import Button from "./Button";
 
 const Reply = () => {
   return (
-    <div className="">
+    <div className="flex justify-between gap-4 m-5 p-5 bg-white rounded-lg shadow-sm">
+      <div>
         <img src="./images/avatars/image-amyrobson.png" alt="avatar" />
-        <textarea name="" id="">
-            
-        </textarea>
-        <button>Reply</button>
+      </div>
+      <textarea
+        className="w-full p-2 border rounded-md focus:outline-none focus:ring-[1px] focus:ring-primary-moderateBlue"
+        rows={3}
+        defaultValue={
+          "@amyrobson. yeah. the attention to detail with the design is"
+        }
+      />
+      <div>
+        <Button text="reply" type="active" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Reply
+export default Reply;
